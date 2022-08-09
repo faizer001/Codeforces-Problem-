e291c77bbb867e8853f61aea55466b7e9fc19105
Faizer <99070117+faizer001@users.noreply.github.com>
2022-08-09 10:31:17 -0700

    Add files via upload

---
+++ lab4/1A.cpp
@@ -0,0 +1,11 @@
+//code forces :1A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+double m,n,a;
+cin >> m>>n>>a;
+cout << (long long)ceil(n/a) *(long long)ceil(m/a) <<endl;
+
+return 0;
+}
