d46903b2bea2a2c20e3b9da75d43e76b5f6a4f69
Faizer <99070117+faizer001@users.noreply.github.com>
2022-10-18 07:33:49 -0700

    Add files via upload

---
+++ lab4/codeforce112a.cpp
@@ -0,0 +1,25 @@
+//faizer01
+//codeforces: 112A
+
+#include <iostream>
+
+using namespace std;
+string a, b;
+int main() {
+    cin >> a >> b;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] < 92) {
+            a[i] += 32;
+        }
+        if (b[i] < 92) {
+            b[i] += 32;
+        }
+    }
+    if (a < b) {
+        cout << -1;
+    } else if (a > b) {
+        cout << 1;
+    } else if (a == b) {
+        cout << 0;
+    }
+}
