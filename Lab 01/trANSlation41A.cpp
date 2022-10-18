76a83b109e600a396874475a2ad9d88f3164a688
Faizer <99070117+faizer001@users.noreply.github.com>
2022-10-18 07:52:54 -0700

    Add files via upload

---
+++ Lab 01/trANSlation41A.cpp
@@ -0,0 +1,17 @@
+//faizar01
+//problem no :41A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   string s;
+   string s1;
+   cin>>s>>s1;
+   reverse(s.begin(),s.end());
+   if(s==s1)
+        cout<<"YES";
+    else
+        cout<<"NO";
+        return 0;
+}
